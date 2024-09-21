32c33c781d4294c105b495be7e242740098636be
Johannes Bernet <jofb@posteo.de>
2024-09-21 22:26:24 +0200

    Update send mail functions in utils

--- includes/utils.php
+++ includes/utils.php
@@ -7,7 +7,7 @@ class Avf_Forms_Utils
         $subject = '[Aikido Verein Freiburg e.V.] Mitgliedschaftsantrag erhalten';
         $message = "Hallo $vorname,\n\n";
         $message .= "Dein Antrag ist bei uns eingegangen. Wir werden uns in Kürze bei dir melden.\n\n";
-        $message .= "Falls Du Fragen zur Mitgliedschaft hast, schreibe bitte eine Mail an schatzmeister@aikido-freiburg.de.";
+        $message .= "Falls Du Fragen zur Mitgliedschaft hast, schreibe bitte eine Mail an schatzmeister@aikido-freiburg.de. ";
         $message .= "Bei allen anderen Fragen, wende dich gerne an vorstand@aikido-freiburg.de oder sprich uns auf der Matte an.\n\n";
         $message .= "Viele Grüße\n";
         $message .= "Dein Aikido Verein Freiburg e.V.\n";
@@ -16,22 +16,22 @@ class Avf_Forms_Utils
         $admin_email = get_option('admin_email');
         $admin_subject = 'Neuer Mitgliedschaftsantrag eingegangen';
         $admin_message = "Neuer Mitgliedschaftsantrag von $vorname $nachname eingegangen.\n\n";
-        $admin_message .= "Download CSV: " . home_url('/csv-download') . "\n";
+        $admin_message .= "Zur Migliedschaftsverwaltung: " . home_url('/wp-admin/admin.php?page=avf-membership-admin') . "\n";
         wp_mail($admin_email, $admin_subject, $admin_message);
     }
 
-    public static function send_schnupperkurs_confirmation_email($email, $vorname, $nachname, $betrag)
+    public static function send_schnupperkurs_confirmation_email($email, $betrag, $vorname, $nachname, $vorname_kind = null, $nachname_kind = null)
     {
         $subject = '[Aikido Verein Freiburg e.V.] Deine Schnupperkurs-Anmeldung';
         $message = "Hallo $vorname,\n\n";
-        $message .= "Deine Anmeldung ist bei uns eingegangen. Vielen Dank!\n\n";
+        $message .= "Deine Anmeldung " . ($vorname_kind ? "für $vorname_kind " : "") . "ist bei uns eingegangen. Vielen Dank!\n\n";
         $message .= "Bitte zahle die Kursgebühr in Höhe von $betrag € innerhalb von zwei Wochen entweder in bar oder per Überweisung auf unser Konto:\n";
         $message .= "Empfänger: Aikido Verein Freiburg e.V.\n";
         $message .= "IBAN: DE34680900000024401901\n";
         $message .= "BIC: GENODE61FR1\n";
         $message .= "Bank: Volksbank Freiburg\n";
-        $message .= "Verwendungszweck: Schnupperkurs $vorname $nachname\n\n";
-        $message .= "Falls Du Fragen zur Mitgliedschaft hast, schreibe gerne eine Mail an schatzmeister@aikido-freiburg.de.";
+        $message .= "Verwendungszweck: Schnupperkurs " . ($vorname_kind ? "$vorname_kind $nachname_kind\n\n" : "$vorname $nachname\n\n");
+        $message .= "Falls Du Fragen zur Mitgliedschaft hast, schreibe gerne eine Mail an schatzmeister@aikido-freiburg.de. ";
         $message .= "Bei allen anderen Fragen, wende dich gerne an vorstand@aikido-freiburg.de oder sprich uns auf der Matte an.\n\n";
         $message .= "Viele Grüße\n";
         $message .= "Dein Aikido Verein Freiburg e.V.\n";
@@ -39,8 +39,8 @@ class Avf_Forms_Utils
 
         $admin_email = get_option('treasurer_email');
         $admin_subject = "[Aikido Verein Freiburg e.V.] Neue Schnupperkurs-Anmeldung";
-        $admin_message = "Neue Schnupperkurs-Anmeldung von $vorname $nachname eingegangen.\n\n";
-        $admin_message .= "Download CSV: " . home_url('/csv-download') . "\n";
+        $admin_message = "Neue Schnupperkurs-Anmeldung von " . ($vorname_kind ? "$vorname_kind $nachname_kind" : "$vorname $nachname") . " eingegangen.\n\n";
+        $admin_message .= "Zur Migliedschaftsverwaltung: " . home_url('/wp-admin/admin.php?page=avf-membership-admin') . "\n";
         wp_mail($admin_email, $admin_subject, $admin_message);
     }
 
